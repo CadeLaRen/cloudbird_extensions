e27594362a3269c07414d036cbaf821b2d70be5b
Davide Depau <me@davideddu.org>
2015-02-03 18:17:31 +0100

    fix another issue with distutil

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.extension import Extension
 
 setup(
     name = "CloudBird Extensions",
-    ext_modules = [Extension("cloudbird_extensions", ["polygon.c", "smoothaccel.c"])],
+    ext_modules = [Extension("polygon", ["polygon.c"]), Extension("smoothaccel", ["smoothaccel.c"])],
 )
